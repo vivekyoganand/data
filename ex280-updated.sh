00a7ea2e91420a79431f910ed970e5e163b0f05b
Vivek Yoganand <vyoganan@vyoganan.remote.csb>
2021-04-11 14:38:22 +0530

    updated

--- ex280-updated.sh
+++ ex280-updated.sh
@@ -5,9 +5,9 @@ x=$(cat /home/student/kubeadmin-password)
 y=$(cat /home/student/kubeconfig | grep server | awk -F" " '{print $2}'|uniq) 
 oc login -u kubeadmin -p $x $y
 oc new-project project-ques7
-oc adm taint node worker01 key1=value1:NoSchedule
-oc adm taint node worker02 key1=value1:NoSchedule
-oc adm taint node worker03 key1=value1:NoSchedule
+#oc adm taint node worker01 key1=value1:NoSchedule
+#oc adm taint node worker02 key1=value1:NoSchedule
+#oc adm taint node worker03 key1=value1:NoSchedule
 oc new-app --name app1 httpd 
 
 oc new-project project-ques8
